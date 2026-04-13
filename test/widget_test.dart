52659eb6c09acdb73770e8edff30d2e2807daa97
takruri23 <takruri23@Gmail.com>
2026-04-13 21:57:44 +0300

    day3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_test_application_1/main.dart';
+import 'package:flutter_test_application_1/core/routing/app_router.dart';
+import 'package:flutter_test_application_1/doc_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(DocApp(appRouter: AppRouter()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
